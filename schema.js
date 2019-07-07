c808ab53dbc3c7fdd718b65e3d8508d91db260ae
bogdanned <bogdan.nedelcu@lokallead.de>
2019-07-07 14:24:53 +0200

    added schema file

---
+++ schema.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { makeExecutableSchema } = require('graphql-tools');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+export default makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
---
+++ server.js
@@ -0,0 +1,9 @@
+const schema = require('./schema')
+
+const server = new ApolloServer({
+  schema
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
